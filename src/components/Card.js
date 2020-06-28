e8cedc85881cd91817edcc93de4dd0a4ddcf2ad5
Preeti Ambekar <preeti@Preetis-MBP.lan>
2020-06-28 17:42:58 +0200

    Grid and galleries interactions

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,19 @@ import React from 'react';
 import juice from './juice.jpg';
 import './Card.css';
 
-const Card = () => (
-  <div className="card">
-    <img src={juice} alt="j§uice" className="card-image" />
+const Card = (props) => (
+  <div className={props.card.animation}>
+    <div className="front" onClick={() => props.showBack(props.card)}>
+    <img src={juice} alt="juice" className="card-image" />
     <div className="container">
       <h3>Vitamin Juice <span className="price">$24.99</span></h3>
       <p>Need a jum on your vitamins while drinking? Tired of popping pills? 
       Drink our vitamin enhanced juice, available in several flavours.</p>
+    </div>
+    </div>
+    <div className="container-back back" onClick={() => props.showFront(props.card)}>
+      <h3>Vitamin Juice <span className="price">$24.99</span></h3>
+      <p>{props.card.description}</p>
     </div>
   </div>
 );
